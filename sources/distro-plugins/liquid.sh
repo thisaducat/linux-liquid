2bf2f2c8dfa8f0a1807271c0fb088642d46211ad
thisaducat <131821398+thisaducat@users.noreply.github.com>
2023-04-27 19:20:31 +0300

    Create liquid.sh

---
+++ sources/distro-plugins/liquid.sh
@@ -0,0 +1,5 @@
+DISTRO_NAME="Liquid"
+DISTRO_COMMENT="rootFS for Liquid"
+
+TARBALL_URL['aarch64']="https://github.com/turkishcydia/ROSA/releases/download/rosafs/liquid-aarch64.tar.xz"
+TARBALL_SHA256['aarch64']="010ac9c5a97c5b984ae4e94639e6e6f1b4a7cac08627862fbdffd151fcc67a38"

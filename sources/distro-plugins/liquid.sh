7a291eb3253fbec780a8438baa2f8bc3c686c753
thisaducat <131821398+thisaducat@users.noreply.github.com>
2023-04-28 18:44:02 +0300

    Update liquid.sh

--- sources/distro-plugins/liquid.sh
+++ sources/distro-plugins/liquid.sh
@@ -1,5 +1,5 @@
 DISTRO_NAME="Liquid"
 DISTRO_COMMENT="rootFS for Liquid"
 
-TARBALL_URL['aarch64']="https://github.com/turkishcydia/ROSA/releases/download/rosafs/liquid-aarch64.tar.xz"
-TARBALL_SHA256['aarch64']="010ac9c5a97c5b984ae4e94639e6e6f1b4a7cac08627862fbdffd151fcc67a38"
+TARBALL_URL['aarch64']="https://github.com/thisaducat/linux-liquid/releases/download/Liquid/liquid-aarch64.tar.xz"
+TARBALL_SHA256['aarch64']="073aeacac16d256dbd979442c80d63acf3c0bb6f60e3d9813105d7717becf93c"
